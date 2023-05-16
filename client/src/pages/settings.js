00269ccba1afddb242db48026aacdbe9d928c897
Daneshwari C Pattanashetti <pattanashettidanu7@gmail.com>
2023-05-16 23:31:50 +0530

    updated proflie settings

--- client/src/pages/settings.js
+++ client/src/pages/settings.js
@@ -1,14 +1,32 @@
-import React from "react";
-import { useState} from "react";
+import React,{ useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+import {ethers,utils} from "ethers";
 import "./settings.css";
-import { Input, Upload } from "@web3uikit/core";
+import { Input, Upload, Loading, useNotification } from "@web3uikit/core";
+import { Web3Storage } from "web3.storage";
 
 const Settings = ({contract}) => {
+  
+  const notification = useNotification();
+  const userName = JSON.parse(localStorage.getItem("userName"));
+  const userBio = JSON.parse(localStorage.getItem("userBio"));
+  const userImage = JSON.parse(localStorage.getItem("userImage"));
+  const userBanner = JSON.parse(localStorage.getItem("userBanner"));
+
   const [profileFile, setProfileFile] = useState();
   const [bannerFile, setBannerFile] = useState();
-  const [name, setName] = useState();
-  const [bio, setBio] = useState();
+  const [name, setName] = useState(userName);
+  const [bio, setBio] = useState(userBio);
+  const [loading, setLoading] = useState(false);
+  let profileUploadedUrl = userImage;
+  let bannerUploadedUrl = userBanner;
+
+  async function storeFile (selectedFile) {
+    const client = new Web3Storage({token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDAxYjlmMTJjYjIzODRiMjBjNjI1ODVDNjFFNDlhRWNjNDczNDE5QTAiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODQyMDA5OTU3NjMsIm5hbWUiOiJUd2l0dGVyIEFQSSJ9.1rdk7balAW5YEevUwsVA35K4DTAXQGqIYjxgXQnFvnc"});
+    const rootCid = await client.put(selectedFile);
+     const ipfsUploadedUrl = `https://${rootCid}.ipfs.dweb.link/${selectedFile[0].name}`;
+    return ipfsUploadedUrl;
+  }
 
   const bannerHandler = (event) => {
     if (event != null) {
@@ -21,6 +39,53 @@ const Settings = ({contract}) => {
       setProfileFile(event);
     }
   };
+  
+  // it rerenders the page whenever a loading state changes 
+  useEffect(() =>{
+
+  },[loading]);
+
+  async function updateProfile(){
+    setLoading(true);
+    if(profileFile != null){
+      let newprofileUploadedUrl = await storeFile([profileFile]);
+      profileUploadedUrl = newprofileUploadedUrl;
+     }
+
+     if(bannerFile!= null){
+      let newBannerUploadedUrl = await storeFile([bannerFile]);
+      bannerUploadedUrl = newBannerUploadedUrl;
+     }
+
+     const transaction= await contract.updateUser(name,bio,profileUploadedUrl,bannerUploadedUrl);
+     await transaction.wait();
+
+     window.localStorage.setItem(
+      "userName",
+      JSON.stringify(name)
+    );
+    window.localStorage.setItem(
+      "userBio",
+      JSON.stringify(bio)
+    );
+    window.localStorage.setItem(
+      "userImage",
+      JSON.stringify(profileUploadedUrl)
+    );
+    window.localStorage.setItem(
+      "userBanner",
+      JSON.stringify(bannerUploadedUrl)
+    );
+    
+    notification({
+      type: 'success',
+      title: 'profile updated successfully',
+      position: 'topR'
+    });
+
+    setLoading(false);
+
+  }
 
   return (
     <>
@@ -31,6 +96,7 @@ const Settings = ({contract}) => {
           width="100%"
           labelBgColor="#141d26"
           onChange={(e) => setName(e.target.value)}
+          value={userName}
         />
         <Input
           label="Bio"
@@ -38,12 +104,13 @@ const Settings = ({contract}) => {
           width="100%"
           labelBgColor="#141d26"
           onChange={(e) => setBio(e.target.value)}
+          value={userBio}
         />
         <div className="pfp">Change Profile Image</div>
         <Upload onChange={profileHandler} />
         <div className="pfp">Change Banner Image</div>
         <Upload onChange={bannerHandler} />
-        <div className="save">Save</div>
+        {loading? <div className="save">loading</div> : <div className="save" onClick={updateProfile}>Save</div>}       
       </div>
     </>
   );
